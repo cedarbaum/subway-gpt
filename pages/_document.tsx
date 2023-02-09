9f7b95d2357c0c4039c393443737cdbb12147f75
Samuel Cedarbaum <scedarbaum@gmail.com>
2023-02-09 18:47:44 -0500

    Add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,6 +21,10 @@ class MyDocument extends Document {
             name="twitter:description"
             content="Find NYC Subway directions with GPT-3!"
           />
+          <link
+            rel="icon"
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚇</text></svg>"
+          />
         </Head>
         <body className="bg-black">
           <Main />
