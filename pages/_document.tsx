efb8568828bd2038ecdef145113c692ba2523bb4
Samuel Cedarbaum <scedarbaum@gmail.com>
2023-02-09 00:49:26 -0500

    Update _document metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,27 +7,19 @@ class MyDocument extends Document {
         <Head>
           <meta
             name="description"
-            content="Generate your next Twitter bio in seconds."
+            content="Find NYC Subway directions with GPT-3!"
           />
           <meta property="og:site_name" content="twitterbio.com" />
           <meta
             property="og:description"
-            content="Generate your next Twitter bio in seconds."
+            content="Find NYC Subway directions with GPT-3!"
           />
-          <meta property="og:title" content="Twitter Bio Generator" />
+          <meta property="og:title" content="SubwayGPT" />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Twitter Bio Generator" />
+          <meta name="twitter:title" content="SubwayGPT" />
           <meta
             name="twitter:description"
-            content="Generate your next Twitter bio in seconds."
-          />
-          <meta
-            property="og:image"
-            content="https://twitterbio.com/og-image.png"
-          />
-          <meta
-            name="twitter:image"
-            content="https://twitterbio.com/og-image.png"
+            content="Find NYC Subway directions with GPT-3!"
           />
         </Head>
         <body className="bg-black">
